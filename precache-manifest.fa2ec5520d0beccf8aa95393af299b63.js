ea29a6b7c65978e259b017d6ceff16d62bf08c19
AtlasX1 <pavlo.sergij@ukr.net>
2021-04-29 15:06:16 +0300

    Updates

---
+++ precache-manifest.fa2ec5520d0beccf8aa95393af299b63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fefbb86480e19d52e09578b99e96461",
+    "url": "/Diploma-front/index.html"
+  },
+  {
+    "revision": "761cfc88a8755a0009f8",
+    "url": "/Diploma-front/static/css/main.b77ee218.chunk.css"
+  },
+  {
+    "revision": "874eddd0929d086ad91a",
+    "url": "/Diploma-front/static/js/2.5cb810df.chunk.js"
+  },
+  {
+    "revision": "5866178b5721e25ca08aad145c7bd786",
+    "url": "/Diploma-front/static/js/2.5cb810df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761cfc88a8755a0009f8",
+    "url": "/Diploma-front/static/js/main.7cbdea5a.chunk.js"
+  },
+  {
+    "revision": "9ad7ef23d5ed39159589",
+    "url": "/Diploma-front/static/js/runtime-main.41e84ecb.js"
+  }
+]);
