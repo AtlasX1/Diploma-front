a8538bfb44d0cc841ef9aae78f22088ca5543f4e
AtlasX1 <pavlo.sergij@ukr.net>
2021-05-12 02:29:08 +0300

    Updates

--- precache-manifest.657a963de9aa1015063fc45e172a51e1.js
+++ precache-manifest.657a963de9aa1015063fc45e172a51e1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a36586885d73f136c98ec64150ec7026",
+    "revision": "fd71a5961fee2fb558ef624e8906c3e1",
     "url": "/Diploma-front/index.html"
   },
   {
-    "revision": "91882c20aea213ab50f0",
+    "revision": "b98333b072e875f26cf6",
     "url": "/Diploma-front/static/css/main.578cfb10.chunk.css"
   },
   {
-    "revision": "3dbda1ea5572b00887bb",
-    "url": "/Diploma-front/static/js/2.8091f38f.chunk.js"
+    "revision": "54ccd0b0489284582d37",
+    "url": "/Diploma-front/static/js/2.91ed23f3.chunk.js"
   },
   {
     "revision": "5866178b5721e25ca08aad145c7bd786",
-    "url": "/Diploma-front/static/js/2.8091f38f.chunk.js.LICENSE.txt"
+    "url": "/Diploma-front/static/js/2.91ed23f3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "91882c20aea213ab50f0",
-    "url": "/Diploma-front/static/js/main.abf8372b.chunk.js"
+    "revision": "b98333b072e875f26cf6",
+    "url": "/Diploma-front/static/js/main.f56f81ae.chunk.js"
   },
   {
     "revision": "9ad7ef23d5ed39159589",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Diploma-front/precache-manifest.fe926cc9206a348f1793b53abcb43ed7.js"
+  "/Diploma-front/precache-manifest.657a963de9aa1015063fc45e172a51e1.js"
 );
 
 self.addEventListener('message', (event) => {
