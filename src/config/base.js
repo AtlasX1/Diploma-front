d92b6f4dcb59e88b8c92a7abb68f00c28153874e
AtlasX1 <pavlo.sergij@ukr.net>
2021-05-11 14:27:39 +0300

    Fix url

--- src/config/base.js
+++ src/config/base.js
@@ -1,2 +1,3 @@
 // export const BASE_API_URL = "http://localhost:5000";
-export const BASE_API_URL = "https://umlmodeler.herokuapp.com";
+export const BASE_API_URL =
+  "https://cors-anywhere.herokuapp.com/https://umlmodeler.herokuapp.com";
